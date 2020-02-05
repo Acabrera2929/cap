0656695a47465320d76c99e2afd13f04f9493a06
Andre Cabrera <acabrera@sb.cordblood.net>
2020-02-05 15:46:00 -0800

    imported pandas and added numpy with print stateme

--- python_code.py
+++ python_code.py
@@ -1,1 +1,5 @@
-print 'Hello world!'
+# This notebook will be mainly used for the purpose of this capstone project
+
+import pandas as pandas
+import numpy as np 
+print 'Hello Capstone Project Course'
